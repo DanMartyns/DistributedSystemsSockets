23d71aca14d5cf60d9564947a797fb667394bc66
Daniel Martins <danielmcoelho@ua.pt>
2019-04-02 15:57:03 +0100

    Manager Entity : updated

--- Manager/src/Communication/Message.java
+++ Manager/src/Communication/Message.java
@@ -5,10 +5,41 @@
  */
 package Communication;
 
+import EntitiesState.ManagerState;
+import java.io.Serializable;
+
 /**
  *
  * @author danielmartins
  */
-public class Message {
+public class Message implements Serializable {
+
+    /**
+     * Serial version of the class. Format used is XXYYZZ (classNumber-praticClassNumber-groupNumber)
+     */
+    private final long serialVersionUID = 010401L;
     
+    private MessageType type;
+    
+    private ManagerState state;
+    
+    public Message(){
+        type = null;
+        state = null;
+    }
+    
+    public Message(MessageType type){
+        this();
+        this.type = type;
+    }
+    
+    public Message(MessageType type, int choice){
+        this();
+        this.type = type;
+        
+//        switch(type){
+//            case  
+//        }
+                
+    }
 }
